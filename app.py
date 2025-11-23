3a73556e2ff5c808529bd998ca70c130fca5470e
Mushimuche <palabonrui@gmail.com>
2025-11-23 11:00:11 +0800

    remove github buttons

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ st.set_page_config(
     layout="wide"
 )
 
+# Hide GitHub icon and deploy button
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # ------------------------------
 # Load Dataset
